3e85657158d00adb7dddc577d5ee57b995781a4c
775116851 <775116851@qq.com>
2015-12-08 16:44:45 +0800

    feng A

---
+++ BankPays/FrontRcvResponse.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BankPays
+{
+    public partial class FrontRcvResponse : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
